a67f7c4e3636cb9ab1842e9de18e81b568c4c530
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2021-12-17 09:39:50 -0800

    Add Notes Schema

--- models/period.js
+++ models/period.js
@@ -1,17 +1,45 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Event
+const noteSchema = new Schema({
+  cramps: {
+    type: Number,
+    min: 0,
+    max: 5,
+    notes: String,
+  },
+  backPain: {
+    type: Number,
+    min: 0,
+    max: 5,
+    notes: String,
+  },
+  appetite: {
+    type: Number,
+    min: 0,
+    max: 1,
+    notes: String,
+  },
+  mood: {
+    type: String,
+    enum: ['Anxious', 'Nervous', 'Tense', 'Depressed', 'Sad', 'Grief', 'Confusion', 'Iritated', 'Angry', 'Want to punch a hole in the wall'],
+    notes: String,
+  },
+});
+
+
+
 const periodSchema  = new Schema({
   userId: {type: Schema.Types.ObjectId, ref: 'User'},
   name: {
     type: String,
+    enum: ['Bleeding', 'Ovulating',]
   },
   periodDate: {
     type: Date,
     default: new Date()
-  }
-
+  }, 
+  notes: [noteSchema]
 })
 
 module.exports = mongoose.model('Period', periodSchema)
